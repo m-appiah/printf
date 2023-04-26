0c9f32e73ef5a58a2339243608bd5b5cd0171187
m-appiah <mappiahca@gmail.com>
2023-04-25 20:31:27 -0700

    binary

---
+++ binary.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * binary - print binary
+ * @n: output unsigned int
+ *
+ * Return: nothing
+ */
+void binary(unsigned int n)
+{
+	if (n > 1)
+		binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
+/**
+ * _printf - print formated string
+ * @format: output format
+ *
+ * Return:0
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int output = 0;
+
+    va_start(args, format);
+
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+
+            if (*format == 'b')
+            {
+                unsigned int n = va_arg(args, unsigned int);
+                binary(n);
+                output += sizeof(unsigned int) * CHAR_BIT;
+	    }
+        }
+        else
+        {
+            _putchar(*format);
+            output++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+
+    return (output);
+}
