7b13f2ad650865bf3087c897980abfdd914fb77e
m-appiah <mappiahca@gmail.com>
2023-04-25 19:15:00 -0700

    Education is when you read the fine print. Experience is what you get if you don't

--- printf.c
+++ printf.c
@@ -32,6 +32,32 @@ int _printf(const char *format, ...)
 					putchar('%');
 					count++;
 					break;
+				case 'd':
+					count += printf("%d", va_arg(args, int));
+					break;
+				case 'i':
+					count += printf("%i", va_arg(args, int));
+					break;
+				case 'u':
+					count += printf("%u", va_arg(args, unsigned int));
+					break;
+				case 'o':
+					count += printf("%o", va_arg(args, unsigned int));
+					break;
+				case 'x':
+					count += printf("%x", va_arg(args, unsigned int));
+					break;
+				case 'X':
+					count += printf("%X", va_arg(args, unsigned int));
+					break;
+				case 'p':
+					count += printf("%p", va_arg(args, void *));
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					count += 2;
+					break;
 			}
 		}
 		else
